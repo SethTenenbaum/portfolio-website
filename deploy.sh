d0f987ab458d877a3fbbd4aeff81b49fb38616fb
Seth Tenenbaum <sethtenenbaum@Seths-MacBook-Pro.local>
2025-03-01 15:20:39 -0500

    update home area

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build the Angular project
+ng build --configuration production --output-path docs --base-href /portfolio-website/
+
+# Move the contents of the 'browser' directory to 'docs'
+mv docs/browser/* docs/
+
+# Remove the 'browser' directory
+rm -r docs/browser
+
+# Copy 'index.html' to '404.html'
+cp docs/index.html docs/404.html
+
+# Deploy to GitHub Pages
+npx angular-cli-ghpages --dir=docs
